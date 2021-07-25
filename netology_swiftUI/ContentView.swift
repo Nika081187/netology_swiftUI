ce8792250bd8c278812ed43d2796fb10914ceccb
Veronika Milchakova <v.milchakova@corp.mail.ru>
2021-07-25 18:03:16 +0300

    first commit

---
+++ netology_swiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  netology_swiftUI
+//
+//  Created by v.milchakova on 25.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ netology_swiftUI/netology_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  netology_swiftUIApp.swift
+//  netology_swiftUI
+//
+//  Created by v.milchakova on 25.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct netology_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
